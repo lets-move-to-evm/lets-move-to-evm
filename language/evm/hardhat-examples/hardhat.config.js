9d0b0c57063886f4de4d34f48cc120d578bb64a1
Let's Move To EVM <empty>
2025-01-23 21:27:30 +0100

    Hardhat config fix

--- language/evm/hardhat-examples/hardhat.config.js
+++ language/evm/hardhat-examples/hardhat.config.js
@@ -20,12 +20,12 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 // a new App in its dashboard, and replace "KEY" with its key
 const ALCHEMY_API_KEY_FOR_ROPSTEN = 'KEY1';
 const ALCHEMY_API_KEY_FOR_RINKEBY = 'KEY2';
-const ALCHEMY_API_KEY_FOR_SEPOLIA = process.env.ALCHEMY_API_KEY_FOR_SEPOLIA;
+const ALCHEMY_API_KEY_FOR_SEPOLIA = 'KEY3';
 // Replace this private key with your Ropsten account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = 'process.env.PRIVATE_KEY';
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -49,19 +49,17 @@ module.exports = {
         // opcodes: ['MLOAD', 'MSTORE'],
     },
     networks: {
-        ropsten: {
-            url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY_FOR_ROPSTEN}`,
-            accounts: [`${PRIVATE_KEY}`],
-        },
-        rinkeby: {
-            url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY_FOR_RINKEBY}`,
-            accounts: [`${PRIVATE_KEY}`],
-            // gas: 4250274,
-            // gasPrice: 2500000016
-        },
-        sepolia: {
-            url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_FOR_SEPOLIA}`,
-            accounts: [`${PRIVATE_KEY}`],
-        },
+        // ropsten: {
+        //     url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY_FOR_ROPSTEN}`,
+        //     accounts: [`${PRIVATE_KEY}`],
+        // },
+        // rinkeby: {
+        //     url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY_FOR_RINKEBY}`,
+        //     accounts: [`${PRIVATE_KEY}`],
+        // },
+        // sepolia: {
+        //     url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_FOR_SEPOLIA}`,
+        //     accounts: [`${PRIVATE_KEY}`],
+        // },
     },
 };
